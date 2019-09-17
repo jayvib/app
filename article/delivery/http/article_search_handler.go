2cfe2d009178706d5f80d61341de6c1165588113
jayvib <jayson.vibandor@gmail.com>
2019-09-17 22:34:34 +0800

    initial implementation

--- article/delivery/http/article_search_handler.go
+++ article/delivery/http/article_search_handler.go
@@ -1,3 +1,57 @@
 package http
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jayvib/app/article"
+	"github.com/jayvib/app/internal/app/http/httputil"
+	"github.com/jayvib/app/internal/app/search"
+	"github.com/jayvib/app/log"
+	"net/http"
+	"strconv"
+)
+
 // Please Implement Me
+func RegisterArticleSearchHandler(r gin.IRouter, se article.SearchEngine) {
+	articleHandler := NewArticleSearchHandler(se)
+	r.GET("/search/article", articleHandler.Search)
+}
+
+// NewArticleSearchHandler takes a SearchEngine and return the object containing
+// all the handlers of the article search.
+func NewArticleSearchHandler(se article.SearchEngine) *ArticleSearchHandler {
+	return &ArticleSearchHandler{
+		se: se,
+	}
+}
+
+// ArticleSearchHandler contains all the handlers for all
+// the search operation.
+type ArticleSearchHandler struct {
+	se article.SearchEngine
+}
+
+// Search is a handler that performs uri query.
+func (a *ArticleSearchHandler) Search(c *gin.Context) {
+	query := c.Query("q")
+	sizeStr := c.DefaultQuery("size", "10")
+	fromStr := c.DefaultQuery("from", "0")
+	log.Infof("Query: %s Size: %s From: %s", query, sizeStr, fromStr)
+
+	parseInt := func(s string) int {
+		v, _ := strconv.ParseInt(s, 10, 64)
+		return int(v)
+	}
+
+	input := search.Input{
+		Query: query,
+		Size: parseInt(sizeStr),
+		From: parseInt(fromStr),
+	}
+	ctx := c.Request.Context()
+	result, err := a.se.Search(ctx, input)
+	if err != nil {
+		httputil.HandleSearchError(c, err)
+	}
+	c.JSON(http.StatusOK, result)
+}
+
--- article/delivery/http/article_search_handler_test.go
+++ article/delivery/http/article_search_handler_test.go
@@ -1,3 +1,50 @@
+// +build unit
+
 package http
 
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/jayvib/app/article/mocks"
+	"github.com/jayvib/app/internal/app/search"
+	"github.com/jayvib/app/model"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"testing"
+)
+
 // TODO: Please Implement me
+func TestSearch(t *testing.T) {
+	r := gin.Default()
+	searchEngineMock := new(mocks.SearchEngine)
+
+	expected := &search.Result {
+		TotalHits: 2,
+		Data: []interface{}{
+			&model.Article{
+				ID: "uniqueid1",
+				Title: "Testing 1",
+				Content: "This is a content body for testing 1",
+			},
+			&model.Article{
+				ID: "uniqueid2",
+				Title: "Testing 2",
+				Content: "This is a content body for testing 2",
+			},
+		},
+	}
+
+	searchEngineMock.On("Search", mock.Anything, mock.AnythingOfType("search.Input"))	.
+		Return(expected, nil).Once()
+
+	RegisterArticleSearchHandler(r,  searchEngineMock)
+	w := performRequest(r, http.MethodGet, "/search/article?q=testing", nil)
+	require.Equal(t, http.StatusOK, w.Code)
+
+	var got search.Result
+	err := json.NewDecoder(w.Body).Decode(&got)
+	require.NoError(t, err)
+	assert.Equal(t, expected.TotalHits, got.TotalHits)
+}
