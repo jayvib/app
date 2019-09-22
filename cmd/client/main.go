5968149dc8710cd422d5ee251d4c85be11fd6664
jayvib <jayson.vibandor@gmail.com>
2019-09-22 09:39:31 +0800

    modified some files

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,7 +3,11 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	router.Static("/", "../../web/assets")
+	router.Static("/assets", "web/app/assets")
+	router.Static("/tutorial/headFirstHTMLCSS/", "web/tutorial/headFirstHTMLCSS")
+	router.Static("/tutorial/headFirstLounge", "web/tutorial/headFirstHTMLCSS/headFirstLounge")
+	router.Static("/tutorial/starbuzz/", "web/tutorial/headFirstHTMLCSS/starbuzz")
 	router.Run(":8080")
 }
