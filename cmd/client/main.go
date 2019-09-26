9af9a6824e62d989df6c9f4fbd3c1347b0c432b2
jayvib <jayson.vibandor@gmail.com>
2019-09-26 19:38:41 +0800

    added more files

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,7 +7,8 @@ func main() {
 	router := gin.Default()
 	router.Static("/assets", "web/app/assets")
 	router.Static("/tutorial/headFirstHTMLCSS/", "web/tutorial/headFirstHTMLCSS")
-	router.Static("/tutorial/headFirstLounge", "web/tutorial/headFirstHTMLCSS/headFirstLounge")
+	router.Static("/tutorial/headFirstLounge/", "web/tutorial/headFirstHTMLCSS/headFirstLounge")
 	router.Static("/tutorial/starbuzz/", "web/tutorial/headFirstHTMLCSS/starbuzz")
+	router.Static("/tutorial/journal", "web/tutorial/headFirstHTMLCSS/journal")
 	router.Run(":8080")
 }
