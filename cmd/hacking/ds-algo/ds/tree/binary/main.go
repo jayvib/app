322d08602482f3859efbbf809ce62aac25ea7c6c
jayvib <jayson.vibandor@gmail.com>
2019-09-29 14:24:06 +0800

    add more files

--- cmd/hacking/ds-algo/ds/tree/binary/main.go
+++ cmd/hacking/ds-algo/ds/tree/binary/main.go
@@ -1,22 +1,63 @@
 package main
 
-type node struct {
-	data  int
-	left  *node
-	right *node
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Tree struct {
+	Root *Node `json:"root"`
+}
+
+func (t *Tree) PrintPostOrder() {
+	printPostOrder(t.Root, "root", "\n")
+	fmt.Println()
+}
+
+func printPostOrder(n *Node, where string, sep string) {
+	if n == nil {
+		return
+	}
+
+	fmt.Print(where, " ", n.Value, sep)
+	printPostOrder(n.Left, "left", "\n\t")
+	printPostOrder(n.Right, "right", "\n\t")
+}
+
+type Node struct {
+	Value int
+	Left  *Node `json:"left"`
+	Right *Node `json:"right"`
+}
+
+func levelOrderBinaryTree(arr []int, start int, size int) *Node {
+	curr := &Node{arr[start], nil, nil}
+	left := 2*start + 1
+	right := 2*start + 2
+	if left < size {
+		curr.Left = levelOrderBinaryTree(arr, left, size)
+	}
+
+	if right < size {
+		curr.Right = levelOrderBinaryTree(arr, right, size)
+	}
+	return curr
+}
+
+func LevelOrderBinaryTree(arr []int) *Tree {
+	tree := new(Tree)
+	tree.Root = levelOrderBinaryTree(arr, 0, len(arr))
+	return tree
 }
 
 func main() {
-	topParent := node{
-		data: 1,
-		left: &node{
-			data:2,
-		},
-		right: &node{
-			data: 3,
-		},
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	t2 := LevelOrderBinaryTree(arr)
+	t2.PrintPostOrder()
+	bite, err := json.Marshal(t2)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	topParent.left.left = &node{data: 4}
-
+	fmt.Println(string(bite))
 }
