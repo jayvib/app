e8463fa9066cb110e84ad4a96d1505dec8133b21
jayvib <jayson.vibandor@gmail.com>
2019-11-03 13:24:31 +0800

    added more files

--- cmd/hacking/ds-algo/ds/stack/v2/slice/stack.go
+++ cmd/hacking/ds-algo/ds/stack/v2/slice/stack.go
@@ -1,15 +1,26 @@
 package slice
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 type StackInt struct {
 	s []int
 }
 
 func (s *StackInt) Pop() int {
-	return 0
+	if s.IsEmpty() {
+		return 0
+	}
+	v := s.s[s.Len()]
+	s.s = s.s[:s.Len()-1]
+	return v
 }
 
 func (s *StackInt) Push(value int) {
-
+	s.s = append(s.s, value)
 }
 
 func (s *StackInt) Len() int {
@@ -18,4 +29,14 @@ func (s *StackInt) Len() int {
 
 func (s *StackInt) IsEmpty() bool {
 	return s.Len() == 0
+}
+
+func (s *StackInt) Print() {
+	s.Fprint(os.Stdout)
+}
+
+func (s *StackInt) Fprint(w io.Writer) {
+	for _, e := range s.s {
+		fmt.Fprintln(w, e)
+	}
 }
